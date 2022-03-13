1e0ceb83d4e8fde79d6d720a6c1f1c8b08fcb0fb
Sam5065 <sam.hilland1@icloud.com>
2022-03-13 16:35:32 +0000

    provides schedule,json file provided from splatnet 2

--- kelpbot/Program.cs
+++ kelpbot/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace kelpbot
 {
@@ -10,7 +12,37 @@ namespace kelpbot
     {
         static void Main(string[] args)
         {
+            int time;
+            time = 0;
+            Console.WriteLine("Current Date & Time {0}", DateTime.Now);
+            Console.ReadLine();
+            
+            string sURL;
+            sURL = "https://splatoon2.ink/data/schedules.json";
+
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(sURL);
+            wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    Console.WriteLine("{0}:{1}", i, sLine);
+            }
+            Console.ReadLine();
+
 
         }
+
     }
 }
